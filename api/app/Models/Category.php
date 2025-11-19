2dd23334b4968f4df063e7a05bf648985e1261f1
Mateo Llerena <mateollerena40@gmail.com>
2025-11-19 16:40:24 -0500

    Creando Controladores para Materia y Categoria

--- api/app/Models/Category.php
+++ api/app/Models/Category.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    protected $table = 'categories';
+
+    protected $fillable = [
+        'name',
+        'status'
+    ];
+
+    public $timestamps = true;
+
+    public function materials()
+    {
+        return $this->hasMany(Material::class, 'category_id');
+    }
 }
--- api/app/Models/Material.php
+++ api/app/Models/Material.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Material extends Model
 {
-    //
+    protected $table = 'materials';
+
+    protected $fillable = [
+        'name',
+        'description',
+        'minimum_stock',
+        'category_id',
+        'status'
+    ];
+
+    public $timestamps = true;
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'category_id');
+    }
 }
