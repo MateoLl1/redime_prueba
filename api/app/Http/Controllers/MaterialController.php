7b53107e74fd40205fbcfb8154009e776264c73c
Mateo Llerena <mateollerena40@gmail.com>
2025-11-19 16:42:44 -0500

    MaterialController Crud

--- api/app/Http/Controllers/MaterialController.php
+++ api/app/Http/Controllers/MaterialController.php
@@ -2,47 +2,47 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Material;
+use App\Http\Resources\MaterialResource;
 use Illuminate\Http\Request;
 
 class MaterialController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        return MaterialResource::collection(
+            Material::with('category')->get()
+        );
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'required|max:50',
+            'description' => 'required',
+            'minimum_stock' => 'required|numeric',
+            'category_id' => 'required|exists:categories,id'
+        ]);
+
+        $material = Material::create($request->all());
+
+        return new MaterialResource($material);
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
+    public function update(Request $request, $id)
     {
-        //
+        $material = Material::findOrFail($id);
+
+        $material->update($request->all());
+
+        return new MaterialResource($material);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
+    public function destroy($id)
     {
-        //
-    }
+        $material = Material::findOrFail($id);
+        $material->delete();
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        return response()->json(['message' => 'deleted']);
     }
 }
